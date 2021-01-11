e3cd43c2252145f9e0d6889a47def166c8c51d88
Josh Maddison <62969771+joshmaddison-IBM@users.noreply.github.com>
2021-01-11 14:36:48 +1100

    Create main.js

---
+++ main.js
@@ -0,0 +1,25 @@
+// requisite MongoDB libraries
+const express = require('express')
+const axios = require('axios')
+var f = require('util').format;
+
+const listenport = 8080 // by default the Route configuration for NJS on OpenShift listens on port 8080
+const webapp = express()
+
+webapp.get('/', (req, res) => {
+    res.send("Hello, World!"); // nothing too complex really
+})
+
+webapp.use(express.json())
+webapp.use(
+    express.urlencoded({
+        extended: true
+      })
+)
+
+webapp.listen(listenport, () => {
+    console.log(`Example app now listening on localhost port ${listenport}`)
+})
+
+
+
